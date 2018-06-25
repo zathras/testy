e521cf71b1a83b21d23813619fdfae989ded4a5f
Bill Foote <billf@jovial.com>
2018-06-24 22:10:05 -0700

    Put javadocs inside a directory within the zip file

--- build/javadocs.sh
+++ build/javadocs.sh
@@ -3,7 +3,10 @@ cd `dirname $0`/..
 echo "Running in `pwd`"
 rm -rf docs/javadocs
 javadoc -d docs/javadocs -sourcepath src edu.calpoly.testy
-cd docs/javadocs
-mkdir -p ../../out
-zip -q -r ../../out/testy-javadocs.zip *
+cd docs
+cp -r javadocs testy-javadocs
+mkdir -p ../out
+rm -f ../out/testy-javadocs.zip
+zip -q -r ../out/testy-javadocs.zip testy-javadocs
+rm -rf testy-javadocs
 echo "Created out/testy-javadocs.zip"
